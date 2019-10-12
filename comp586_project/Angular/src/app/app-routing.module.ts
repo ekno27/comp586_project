66fd44d956802e21dd866f5d3c2378edbd995d64
ekno27 <edgkano@gmail.com>
2019-10-11 18:14:20 -0700

    setup component templates

--- comp586_project/Angular/src/app/app-routing.module.ts
+++ comp586_project/Angular/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {ShelfComponent} from './shelf/shelf.component';
+import {ModalComponent} from './modal/modal.component';
+
 import {AppComponent} from './app.component'
 const routes: Routes = [
   {
@@ -16,7 +18,12 @@ const routes: Routes = [
   {
     path:'login',
     component: LoginComponent
+  },
+  {
+    path:'modal',
+    component: ModalComponent
   }
+  
 ];
 
 @NgModule({
