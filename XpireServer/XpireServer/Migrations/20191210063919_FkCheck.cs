89cf20ff231c5fd04a4bce23f67d94f7d3e5a376
ekno27 <edgkano@gmail.com>
2019-12-10 14:00:20 -0800

    correct side of db

---
+++ XpireServer/XpireServer/Migrations/20191210063919_FkCheck.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace XpireServer.Migrations
+{
+    public partial class FkCheck : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
